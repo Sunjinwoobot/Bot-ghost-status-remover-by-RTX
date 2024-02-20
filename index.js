750074e2d08b1bf6a906dfa021ef5a34ca27fbd7
Sunjinwoobot <159629598+Sunjinwoobot@users.noreply.github.com>
2024-02-20 13:59:10 +0530

    index.js

--- index.js
+++ index.js
@@ -28,15 +28,16 @@ const client = new Client({
 const app = express();
 const port = 3000;
 app.get('/', (req, res) => {
-  res.send('YaY Your Bot Status Changed✨');
+  const imagePath = path.join(__dirname, 'index.html');
+  res.sendFile(imagePath);
 });
 app.listen(port, () => {
   console.log(`🔗 Listening to RTX: http://localhost:${port}`);
-  console.log(`🔗 Powered By RTX`);
+  console.log(`🔗 Replit URL: https://${process.env.REPL_SLUG}.${process.env.REPL_OWNER}.repl.co`);
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = ["Watching /𝗘𝘃𝗮𝗱𝗲"];
 
 
 let currentIndex = 0;
@@ -98,7 +99,7 @@ client.once('ready', () => {
 
   setInterval(() => {
     updateStatusAndSendMessages();
-  }, 10000);
+  }, 5000);
 });
 
 login();
